0301c31175285ed62305d5d8f9ae8160fc40da4b
Vishal Rathod <rathodvishal@johndeere.com>
2017-10-05 18:22:29 +0530

    added method to scan a table using boto3 resource.

--- dynamodb/Dynamodb.py
+++ dynamodb/Dynamodb.py
@@ -7,7 +7,7 @@ class Dynamo:
     client = boto3.client("dynamodb", endpoint_url="http://localhost:8000")
     resource = boto3.resource("dynamodb", endpoint_url="http://localhost:8000")
 
-    def insert_record(self, data, tablename):
+    def insert_record_client(self, data, tablename):
         response = self.client.put_item(
             TableName=tablename,
             Item=data
@@ -17,9 +17,19 @@ class Dynamo:
     def delete_record(self, primary_key, tablename):
         pass
 
-    def scan_table(self, primary_key, tablename):
+    def scan_table_client(self, primary_key, tablename):
         """this needs improvement"""
         response = self.client.scan(
+            TableName=tablename,
+            IndexName=primary_key,
+            Select='ALL_ATTRIBUTES'
+        )
+        return response
+
+    def scan_table_resource(self, primary_key, tablename):
+        """this needs improvement"""
+        table = self.resource.Table(tablename)
+        response = table.scan(
             TableName=tablename,
             IndexName=primary_key,
             Select='ALL_ATTRIBUTES'
@@ -29,7 +39,7 @@ class Dynamo:
     def query_gsi(self, gsi, tablename):
         pass
 
-    def query_table(self, index_name, index_value,
+    def query_table_resource(self, index_name, index_value,
                    primary_key_name,
                    attribute_to_get, tablename):
         """I find querying the table using resource much more simpler than querying through client."""
